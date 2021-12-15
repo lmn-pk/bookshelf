157a3eb7c308f3d718fe38b06637d7598c74c940
Louise Moesgaard Nielsen <lmn@parkeringskompagniet.dk>
2021-12-15 12:53:40 +0100

    extra 1

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -3,19 +3,23 @@ import {jsx} from '@emotion/core'
 
 import * as React from 'react'
 import Tooltip from '@reach/tooltip'
-import {FaSearch} from 'react-icons/fa'
+import {FaSearch, FaTimes} from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
 import {BookRow} from './components/book-row'
 import {client} from './utils/api-client'
+import * as colors from './styles/colors'
 
 function DiscoverBooksScreen() {
   const [status, setStatus] = React.useState('idle')
   const [data, setData] = React.useState(null)
   const [query, setQuery] = React.useState('')
   const [queried, setQueried] = React.useState(false)
+  const [error, setError] = React.useState(null)
+
 
   const isLoading = status === 'loading'
   const isSuccess = status === 'success'
+  const isError = status === 'error'
 
   React.useEffect(() => {
     if (!queried) {
@@ -25,6 +29,9 @@ function DiscoverBooksScreen() {
     client(`books?query=${encodeURIComponent(query)}`).then(responseData => {
       setData(responseData)
       setStatus('success')
+    }, errorData => {
+      setError(errorData)
+      setStatus('error')
     })
   }, [query, queried])
 
@@ -35,46 +42,59 @@ function DiscoverBooksScreen() {
   }
 
   return (
-      <div
-          css={{maxWidth: 800, margin: 'auto', width: '90vw', padding: '40px 0'}}
-      >
-        <form onSubmit={handleSearchSubmit}>
-          <Input
-              placeholder="Search books..."
-              id="search"
-              css={{width: '100%'}}
-          />
-          <Tooltip label="Search Books">
-            <label htmlFor="search">
-              <button
-                  type="submit"
-                  css={{
-                    border: '0',
-                    position: 'relative',
-                    marginLeft: '-35px',
-                    background: 'transparent',
-                  }}
-              >
-                {isLoading ? <Spinner /> : <FaSearch aria-label="search" />}
-              </button>
-            </label>
-          </Tooltip>
-        </form>
+    <div
+      css={{maxWidth: 800, margin: 'auto', width: '90vw', padding: '40px 0'}}
+    >
+      <form onSubmit={handleSearchSubmit}>
+        <Input
+          placeholder="Search books..."
+          id="search"
+          css={{width: '100%'}}
+        />
+        <Tooltip label="Search Books">
+          <label htmlFor="search">
+            <button
+              type="submit"
+              css={{
+                border: '0',
+                position: 'relative',
+                marginLeft: '-35px',
+                background: 'transparent',
+              }}
+            >
+              {isLoading ? (
+                  <Spinner />
+              ) : isError ? (
+                  <FaTimes aria-label="error" css={{color: colors.danger}} />
+                ) : (
+                      <FaSearch aria-label="search" />
+                )}
+            </button>
+          </label>
+        </Tooltip>
+      </form>
 
-        {isSuccess ? (
-            data?.books?.length ? (
-                <BookListUL css={{marginTop: 20}}>
-                  {data.books.map(book => (
-                      <li key={book.id} aria-label={book.title}>
-                        <BookRow key={book.id} book={book} />
-                      </li>
-                  ))}
-                </BookListUL>
-            ) : (
-                <p>No books found. Try another search.</p>
-            )
-        ) : null}
-      </div>
+      {isError ? (
+          <div css={{color: colors.danger}}>
+            <p>There was an error:</p>
+            <pre>{error.message}</pre>
+          </div>
+      ) : null}
+
+      {isSuccess ? (
+        data?.books?.length ? (
+          <BookListUL css={{marginTop: 20}}>
+            {data.books.map(book => (
+              <li key={book.id} aria-label={book.title}>
+                <BookRow key={book.id} book={book} />
+              </li>
+            ))}
+          </BookListUL>
+        ) : (
+          <p>No books found. Try another search.</p>
+        )
+      ) : null}
+    </div>
   )
 }
 
